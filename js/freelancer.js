5c7c840dcc6734a98680f10b1024876ba793ddcd
joshuamorinfilm <joshuamorin@gmail.com>
2019-01-08 15:45:16 -0500

    asd
    asd

--- js/freelancer.js
+++ js/freelancer.js
@@ -27,13 +27,13 @@
   });
 
   // Collapse the navbar when page is scrolled
-  $(window).scroll(function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
-  });
+  //$(window).scroll(function() {
+  //  if ($("#mainNav").offset().top > 100) {
+  //    $("#mainNav").addClass("navbar-shrink");
+   // } else {
+  //    $("#mainNav").removeClass("navbar-shrink");
+  //  }
+  //});
 
   // Floating label headings for the contact form
   $(function() {

25fc53f4ec87d001aa2c8c23e6e642daae46802a
joshuamorinfilm <joshuamorin@gmail.com>
2021-02-05 11:42:50 -0500

    asd

--- js/freelancer.js
+++ js/freelancer.js
@@ -20,20 +20,6 @@
     $('.navbar-collapse').collapse('hide');
   });
 
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 54
-  });
-
-  // Collapse the navbar when page is scrolled
-  //$(window).scroll(function() {
-  //  if ($("#mainNav").offset().top > 100) {
-  //    $("#mainNav").addClass("navbar-shrink");
-   // } else {
-  //    $("#mainNav").removeClass("navbar-shrink");
-  //  }
-  //});
 
   // Floating label headings for the contact form
   $(function() {
